8dd0f549b34c95321ca3b5ea7355bcaee25fc20a
SaafiRahman <saafirah@student.ubc.ca>
2023-08-29 15:47:44 -0700

    added the connection to the API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import Button from 'react-bootstrap/Button';
 
+// fetch("https://pokeapi.co/api/v2")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data);
+// })
 function App() {
+  fetch("https://pokeapi.co/api/v2")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+})
   return (
-    <div>
-      
+    
+    <div className="app">
+      <h1>Generate a random Pokemon</h1>
+      <div className="button">
+      <Button variant="primary">Generate!</Button>
+      </div>
     </div>
   );
 }
