076bebfb33e31df8c4b2bfbc2cd547a54b2776b4
SaafiRahman <saafirah@student.ubc.ca>
2023-08-31 12:54:15 -0700

    added typings for each pokemon

--- src/App.js
+++ src/App.js
@@ -13,10 +13,13 @@ function App() {
 
 const handleClick = () => {
     setPokid(randomNumberInRange(1, 1010));
+    
 };
 
   const [pokemon, setPokemon] = useState(null);
   const [pokid, setPokid] = useState(1);
+
+ 
 
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon/" + pokid + "/")
@@ -24,18 +27,27 @@ const handleClick = () => {
     return res.json();
   })
     .then((data) => {
-      console.log(data);
       setPokemon(data);
+
   })
   }, [pokid]);
   
+ 
+
   return (
     
     <div className="app">
       <h1>Generate a random Pokemon</h1>
       {pokemon && <img src={pokemon.sprites.front_default} width={200}  height={200}/>}
-      
-      
+
+      <div classsName="typespace">
+        {pokemon.types.map((type, key) => (
+          <div key={key}>
+            <span>{type.type.name}</span>
+          </div>
+        ))}
+      </div>
+
       {pokemon &&  <p>{pokemon.name}, {pokid}</p>}
       <div className="button">
       <Button variant="primary" onClick={handleClick}>Generate!</Button>
